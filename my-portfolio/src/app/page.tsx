22ca9e4c7fc6fc9ea89c65b67b684bce230b6fc2
João Vitor <thevitor004@gmail.com>
2024-01-07 02:05:17 -0300

    Nao conseguir colocar um emoji no texto é foda

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
+      <div className="header">
       <Image
         src="/vercel.svg"
         alt="Vecel Logo"
@@ -10,6 +11,27 @@ export default function Home() {
         height={24}
         priority
         />
+      </div>
+      <div>
+        <h1>Hi, i'M João Vitor🏴‍☠️</h1>
+        <h2>Hacker</h2>
+      </div>
+      <div className="experience">
+        <h3>Experience</h3>
+        <p>1 year working in the area of information security at the company CODATA through the AYTY research laboratory at UFPB</p>
+        <div className="experience-time">
+
+        </div>
+        <div className="infos">
+          <h3>Languages</h3>
+          <div className="languages-info">
+          <span>EN - Basic</span>
+          <span>PT-BR - Native Speaker</span>
+          </div>
+          <h3>Education</h3>
+          <span></span>
+        </div>
+      </div>
     </main>
   )
 }
