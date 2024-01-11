00703c669d89d615f075b1527a3ab7aeff69947d
João Vitor <thevitor004@gmail.com>
2024-01-10 23:39:20 -0300

    Header sem erros

--- my-portfolio/src/app/components/header.tsx
+++ my-portfolio/src/app/components/header.tsx
@@ -1,20 +1,19 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-export function header() {
-  return(
+export function Header() {
+  return (
     <div className="header">
-      
       <Image
-      src="/vercel.svg"
-      alt="Vecel Logo"
-      width={100}
-      height={24}
-      priority
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        width={100}
+        height={24}
+        priority
       />
+      <div>
+        <h1>Hi, I'm João Vitor</h1>
+        <h2>Hacker</h2>
+      </div>
     </div>
-    <div>
-      <h1>Hi, i'm João Vitor</h1>
-      <h2>Hacker</h2>
-    </div>
-  )
+  );
 }
