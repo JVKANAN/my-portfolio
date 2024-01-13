f4e14f42793840c8235adc1f1c7d62117e07ba69
João Vitor <thevitor004@gmail.com>
2024-01-12 23:16:56 -0300

    criação do header.scss

--- my-portfolio/src/app/components/header.tsx
+++ my-portfolio/src/app/components/header.tsx
@@ -3,6 +3,11 @@ import Image from 'next/image';
 export function Header() {
   return (
     <div className="header">
+     
+      <div>
+        <h1>Hi, I'm João Vitor</h1>
+        <h2>Hacker</h2>
+      </div>
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
@@ -10,10 +15,6 @@ export function Header() {
         height={24}
         priority
       />
-      <div>
-        <h1>Hi, I'm João Vitor</h1>
-        <h2>Hacker</h2>
-      </div>
     </div>
   );
 }
